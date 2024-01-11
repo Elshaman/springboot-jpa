6bea1fd66c30352a8c86e950ae808bc94194e6c7
Elshaman <cbuitragoo.2023@gmail.com>
2024-01-11 09:35:34 -0500

    application.properties

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `programming_languaje`) VALUES 	(1, 'Danna', 'Velandia', 'JAVA')
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `programming_languaje`) VALUES 	(2, 'Zully', 'Ballesteros', 'PHP')
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `programming_languaje`) VALUES 	(3, 'Vanessa', 'Diaz', 'Python')
+INSERT INTO `persons` (`id`, `first_name`, `last_name`, `programming_languaje`) VALUES 	(4, 'Daniel', 'Alarcon', 'JavaScript')	
+
